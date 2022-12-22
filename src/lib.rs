ebe7aaf1102396c9ac6d34e5afd107d6f1a86d61
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-12-21 18:22:15 -0600

    Feature: Update tests
    Contact: Sam Christy <samchristywork@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -27,13 +27,27 @@ mod tests {
     fn circle() {
         let mut svg = Svg::new(50.0, 50.0);
 
-        svg.add_element("circle cx=\"50\" cy=\"50\" r=\"40\" stroke=\"black\" />");
+        svg.add_element("<circle cx=\"50\" cy=\"50\" r=\"40\" stroke=\"black\" />");
 
-        println!("{}", svg.to_string());
+        assert_eq!(
+            svg.to_string(),
+            "<svg viewBox=\"0 0 50 50\" xmlns=\"http://www.w3.org/2000/svg\">\n\
+            <circle cx=\"50\" cy=\"50\" r=\"40\" stroke=\"black\" />\n\
+            </svg>"
+        );
+    }
+
+    #[test]
+    fn double_circle() {
+        let mut svg = Svg::new(50.0, 50.0);
+
+        svg.add_element("<circle cx=\"50\" cy=\"50\" r=\"40\" stroke=\"black\" />");
+        svg.add_element("<circle cx=\"50\" cy=\"50\" r=\"40\" stroke=\"black\" />");
 
         assert_eq!(
             svg.to_string(),
             "<svg viewBox=\"0 0 50 100\" \
+            <circle cx=\"50\" cy=\"50\" r=\"40\" stroke=\"black\" /> \
             xmlns=\"http://www.w3.org/2000/svg\"></svg>"
         );
     }
@@ -44,9 +58,6 @@ mod tests {
         scattered_tree = scattered_tree / tr(1);
         scattered_tree = scattered_tree / tr(2);
         scattered_tree = scattered_tree / tr(3);
-
-        let x = tree_to_string(&scattered_tree);
-        println!("{}", x);
 
         assert_eq!(scattered_tree, tr(0) / tr(1) / tr(2) / tr(3));
     }
