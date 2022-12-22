a5781710c33b8dbb67b29431cdc2e9b64df5f6b3
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-12-21 20:45:22 -0600

    Feature: Added rectangle shape
    Contact: Sam Christy <samchristywork@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ mod tests {
     use super::*;
     use path::Path;
     use shape::Circle;
+    use shape::Rectangle;
     use svg::Svg;
 
     #[test]
@@ -28,14 +29,28 @@ mod tests {
     fn circle() {
         let mut svg = Svg::new(50.0, 50.0);
 
-        let circle = Circle::new(50.0, 50.0, 40.0);
+        let circle = Circle::new(50.0, 50.0, 40.0, "fill:black");
 
         svg.add_shape(circle);
 
         assert_eq!(
             svg.to_string(),
             "<svg viewBox=\"0 0 50 50\" xmlns=\"http://www.w3.org/2000/svg\">\n\
-            <circle cx=\"50\" cy=\"50\" r=\"40\" stroke=\"black\" />\n\
+            <circle cx=\"50\" cy=\"50\" r=\"40\" style=\"fill:black\" />\n\
+            </svg>"
+        );
+    }
+
+    #[test]
+    fn rectangle() {
+        let mut svg = Svg::new(50.0, 50.0);
+
+        svg.add_shape(Rectangle::new(50.0, 50.0, 10.0, 10.0, "fill:black"));
+
+        assert_eq!(
+            svg.to_string(),
+            "<svg viewBox=\"0 0 50 50\" xmlns=\"http://www.w3.org/2000/svg\">\n\
+            <rectangle x=\"50\" y=\"50\" width=\"10\" height=\"10\" style=\"fill:black\" />\n\
             </svg>"
         );
     }
@@ -44,14 +59,14 @@ mod tests {
     fn double_circle() {
         let mut svg = Svg::new(100.0, 100.0);
 
-        svg.add_shape(Circle::new(50.0, 50.0, 40.0));
-        svg.add_shape(Circle::new(50.0, 50.0, 40.0));
+        svg.add_shape(Circle::new(50.0, 50.0, 40.0, "fill:black"));
+        svg.add_shape(Circle::new(50.0, 50.0, 40.0, "fill:black"));
 
         assert_eq!(
             svg.to_string(),
             "<svg viewBox=\"0 0 100 100\" xmlns=\"http://www.w3.org/2000/svg\">\n\
-            <circle cx=\"50\" cy=\"50\" r=\"40\" stroke=\"black\" />\n\
-            <circle cx=\"50\" cy=\"50\" r=\"40\" stroke=\"black\" />\n\
+            <circle cx=\"50\" cy=\"50\" r=\"40\" style=\"fill:black\" />\n\
+            <circle cx=\"50\" cy=\"50\" r=\"40\" style=\"fill:black\" />\n\
             </svg>"
         );
     }
--- src/shape.rs
+++ src/shape.rs
@@ -1,29 +1,54 @@
 pub trait Shape {
-    fn hello(&self);
     fn to_svg(&self) -> String;
 }
 
-pub struct Circle {
+pub struct Circle<'a> {
     x: f32,
     y: f32,
     r: f32,
+    style: &'a str,
 }
 
-impl Shape for Circle {
-    fn hello(&self) {
-        println!("I am a circle");
-    }
-
+impl Shape for Circle<'_> {
     fn to_svg(&self) -> String {
         format!(
-            "<circle cx=\"{}\" cy=\"{}\" r=\"{}\" stroke=\"black\" />",
-            self.x, self.y, self.r
+            "<circle cx=\"{}\" cy=\"{}\" r=\"{}\" style=\"{}\" />",
+            self.x, self.y, self.r, self.style
         )
     }
 }
 
-impl Circle {
-    pub fn new(x: f32, y: f32, r: f32) -> Self {
-        Self { x, y, r }
+impl<'a> Circle<'a> {
+    pub fn new(x: f32, y: f32, r: f32, style: &'a str) -> Self {
+        Self { x, y, r, style }
+    }
+}
+
+pub struct Rectangle<'a> {
+    x: f32,
+    y: f32,
+    width: f32,
+    height: f32,
+    style: &'a str,
+}
+
+impl Shape for Rectangle<'_> {
+    fn to_svg(&self) -> String {
+        format!(
+            "<rectangle x=\"{}\" y=\"{}\" width=\"{}\" height=\"{}\" style=\"{}\" />",
+            self.x, self.y, self.width, self.height, self.style
+        )
+    }
+}
+
+impl<'a> Rectangle<'a> {
+    pub fn new(x: f32, y: f32, width: f32, height: f32, style: &'a str) -> Self {
+        Self {
+            x,
+            y,
+            width,
+            height,
+            style,
+        }
     }
 }
