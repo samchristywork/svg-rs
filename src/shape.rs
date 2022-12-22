843762f053bc6d7353dc8f622242f4c5629813da
samchristywork <106397573+samchristywork@users.noreply.github.com>
2022-12-22 17:58:01 -0600

    Feature: Add text shape
    Contact: Sam Christy <samchristywork@gmail.com>

--- src/shape.rs
+++ src/shape.rs
@@ -142,3 +142,31 @@ impl<'a> Bezier<'a> {
     }
 }
 
+pub struct Text<'a> {
+    x: f32,
+    y: f32,
+    size: f32,
+    style: &'a str,
+    text: &'a str,
+}
+
+impl Shape for Text<'_> {
+    fn to_svg(&self) -> String {
+        format!(
+            "<text x=\"{}\" y=\"{}\" font-size=\"{}\" style=\"{}\">{}</text>",
+            self.x, self.y, self.size, self.style, self.text
+        )
+    }
+}
+
+impl<'a> Text<'a> {
+    pub fn new(x: f32, y: f32, size: f32, style: &'a str, text: &'a str) -> Self {
+        Self {
+            x,
+            y,
+            size,
+            style,
+            text,
+        }
+    }
+}
