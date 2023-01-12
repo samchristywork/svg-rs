f2b63161673657e99007e0718e359f15c118135b
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-01-11 21:14:52 -0600

    Feature: Added most sample data to "playground"
    Contact: Sam Christy <samchristywork@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -200,15 +200,66 @@ mod tests {
 
     #[test]
     fn playground() {
-        let mut svg = Svg::new(200.0, 200.0);
+        let mut svg = Svg::new(400.0, 300.0);
 
-        let mut path = Path::new(150.0, 0.0);
+        let mut path = Path::new(50.0, 0.0);
 
-        path.line_to(75.0, 200.0);
+        path.line_to(0.0, 100.0);
 
-        path.line_to(225.0, 200.0);
+        path.line_to(100.0, 100.0);
 
         svg.add_element(format!("<path d=\"{}\" />", path).as_str());
+
+        let circle = Circle::new(150.0, 50.0, 40.0, "", "fill:black");
+
+        svg.add_shape(circle);
+
+        svg.add_shape(Rectangle::new(230.0, 30.0, 50.0, 50.0, "", "fill:black"));
+
+        let mut rect = Rectangle::new(330.0, 30.0, 50.0, 50.0, "", "fill:black");
+
+        rect.rounded(8.0, 8.0);
+
+        svg.add_shape(rect);
+
+        let line = Line::new(10.0, 110.0, 80.0, 180.0, "", "stroke:black");
+
+        svg.add_shape(line);
+
+        svg.add_shape(Rectangle::new(130.0, 130.0, 50.0, 50.0, "", "fill:black"));
+
+        svg.add_shape(Rectangle::new(
+            30.0,
+            30.0,
+            10.0,
+            10.0,
+            "translate(200 100) rotate(40 20 40)",
+            "fill:red",
+        ));
+
+        svg.add_shape(Text::new(
+            310.0,
+            110.0,
+            10.0,
+            "",
+            "fill:black",
+            "Hello, World!",
+        ));
+
+        let bezier = Bezier::new(
+            40.0,
+            240.0,
+            80.0,
+            240.0,
+            40.0,
+            280.0,
+            80.0,
+            280.0,
+            "",
+            "stroke:black; fill:none; stroke-width:1",
+        );
+
+        svg.add_shape(bezier);
 
         //println!("\n\n{}\n\n", svg);
 
