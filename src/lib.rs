296c719c244d677ec5a404ed77858194a6b3ed4a
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-11-20 21:09:00 -0600

    test: Update tests to use new API
    The functions in this library now mostly use tuples to represent
    positions instead of using individual arguments for X and Y.

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ pub mod text;
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     use path::Path;
     use shape::Bezier;
     use shape::Circle;
@@ -12,7 +13,6 @@ mod tests {
     use shape::Rectangle;
     use std::fs::File;
     use std::io::{self, Read};
-    use super::*;
     use svg::Svg;
     use text::Text;
 
@@ -33,7 +33,7 @@ mod tests {
     fn circle() {
         let mut svg = Svg::new(50.0, 50.0);
 
-        let circle = Circle::new(50.0, 50.0, 40.0, "", "fill:black");
+        let circle = Circle::new((50.0, 50.0), 40.0, "", "fill:black");
 
         svg.add_shape(circle);
 
@@ -51,7 +51,7 @@ mod tests {
     fn rectangle() {
         let mut svg = Svg::new(50.0, 50.0);
 
-        svg.add_shape(Rectangle::new(30.0, 30.0, 10.0, 10.0, "", "fill:black"));
+        svg.add_shape(Rectangle::new((30.0, 30.0), 10.0, 10.0, "", "fill:black"));
 
         svg.to_file("target/rectangle.svg").unwrap();
 
@@ -68,7 +68,7 @@ mod tests {
     fn rectangle_rounded() {
         let mut svg = Svg::new(50.0, 50.0);
 
-        let mut rect = Rectangle::new(30.0, 30.0, 10.0, 10.0, "", "fill:black");
+        let mut rect = Rectangle::new((30.0, 30.0), 10.0, 10.0, "", "fill:black");
 
         rect.rounded(2.0, 2.0);
 
@@ -89,7 +89,7 @@ mod tests {
     fn line() {
         let mut svg = Svg::new(50.0, 50.0);
 
-        let line = Line::new(10.0, 10.0, 20.0, 20.0, "", "stroke:black");
+        let line = Line::new((10.0, 10.0), (20.0, 20.0), "", "stroke:black");
 
         svg.add_shape(line);
 
@@ -109,14 +109,10 @@ mod tests {
         let mut svg = Svg::new(50.0, 50.0);
 
         let bezier = Bezier::new(
-            10.0,
-            10.0,
-            20.0,
-            10.0,
-            10.0,
-            20.0,
-            20.0,
-            20.0,
+            (10.0, 10.0),
+            (20.0, 10.0),
+            (10.0, 20.0),
+            (20.0, 20.0),
             "",
             "stroke:black; fill:none; stroke-width:.1",
         );
@@ -139,8 +135,7 @@ mod tests {
         let mut svg = Svg::new(50.0, 50.0);
 
         svg.add_shape(Text::new(
-            10.0,
-            10.0,
+            (10.0, 10.0),
             2.0,
             "",
             "fill:black",
@@ -160,8 +155,8 @@ mod tests {
     fn double_circle() {
         let mut svg = Svg::new(100.0, 100.0);
 
-        svg.add_shape(Circle::new(50.0, 50.0, 40.0, "", "fill:black"));
-        svg.add_shape(Circle::new(50.0, 50.0, 40.0, "", "fill:black"));
+        svg.add_shape(Circle::new((50.0, 50.0), 40.0, "", "fill:black"));
+        svg.add_shape(Circle::new((50.0, 50.0), 40.0, "", "fill:black"));
 
         svg.to_file("target/double_circle.svg").unwrap();
 
@@ -178,11 +173,10 @@ mod tests {
     fn transform() {
         let mut svg = Svg::new(100.0, 100.0);
 
-        svg.add_shape(Rectangle::new(30.0, 30.0, 10.0, 10.0, "", "fill:black"));
+        svg.add_shape(Rectangle::new((30.0, 30.0), 10.0, 10.0, "", "fill:black"));
 
         svg.add_shape(Rectangle::new(
-            30.0,
-            30.0,
+            (30.0, 30.0),
             10.0,
             10.0,
             "rotate(40 20 40)",
@@ -211,28 +205,27 @@ mod tests {
     fn misc() {
         let mut svg = Svg::new(400.0, 260.0);
 
-        let mut path = Path::new(50.0, 0.0);
-        path.line_to(0.0, 100.0);
-        path.line_to(100.0, 100.0);
+        let mut path = Path::new((50.0, 0.0));
+        path.line_to((0.0, 100.0));
+        path.line_to((100.0, 100.0));
         svg.add_element(format!("<path d=\"{}\" />", path).as_str());
 
-        let circle = Circle::new(150.0, 50.0, 40.0, "", "fill:red");
+        let circle = Circle::new((150.0, 50.0), 40.0, "", "fill:red");
         svg.add_shape(circle);
 
-        svg.add_shape(Rectangle::new(230.0, 30.0, 50.0, 50.0, "", "fill:orange"));
+        svg.add_shape(Rectangle::new((230.0, 30.0), 50.0, 50.0, "", "fill:orange"));
 
-        let mut rect = Rectangle::new(330.0, 30.0, 50.0, 50.0, "", "fill:yellow");
+        let mut rect = Rectangle::new((330.0, 30.0), 50.0, 50.0, "", "fill:yellow");
         rect.rounded(8.0, 8.0);
         svg.add_shape(rect);
 
-        let line = Line::new(10.0, 110.0, 80.0, 180.0, "", "stroke:green");
+        let line = Line::new((10.0, 110.0), (80.0, 180.0), "", "stroke:green");
         svg.add_shape(line);
 
-        svg.add_shape(Rectangle::new(130.0, 130.0, 50.0, 50.0, "", "fill:blue"));
+        svg.add_shape(Rectangle::new((130.0, 130.0), 50.0, 50.0, "", "fill:blue"));
 
         svg.add_shape(Rectangle::new(
-            30.0,
-            30.0,
+            (30.0, 30.0),
             10.0,
             10.0,
             "translate(200 100) rotate(40 20 40)",
@@ -240,8 +233,7 @@ mod tests {
         ));
 
         svg.add_shape(Text::new(
-            310.0,
-            110.0,
+            (310.0, 110.0),
             10.0,
             "",
             "fill:violet",
@@ -249,14 +241,10 @@ mod tests {
         ));
 
         let bezier = Bezier::new(
-            40.0,
-            200.0,
-            200.0,
-            200.0,
-            140.0,
-            240.0,
-            300.0,
-            240.0,
+            (40.0, 200.0),
+            (200.0, 200.0),
+            (140.0, 240.0),
+            (300.0, 240.0),
             "",
             "stroke:black; fill:none; stroke-width:1",
         );
@@ -264,7 +252,7 @@ mod tests {
 
         svg.to_file("target/shapes.svg").unwrap();
 
-        let contents=read_file_to_string("sample/shapes.svg").unwrap();
+        let contents = read_file_to_string("sample/shapes.svg").unwrap();
         assert_eq!(contents, svg.to_string());
     }
 }
